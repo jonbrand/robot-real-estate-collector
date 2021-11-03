8c18fe71cca5a5d90183acece42851b8083e7aff
Tsuyusk <rafaeldesenhos99@gmail.com>
2021-11-03 15:59:17 -0300

    Add real estates scrapping and partially add nexxt page functionatlity

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false});
+  let scrapUntilPage = 5;
+
+  const data = {};
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
   await page.goto('https://www.zapimoveis.com.br/venda/');
-  await page.evaluate(() => {
-    const nodeList = document.querySelectorAll('div section a');
 
-    const imgArray = [...nodeList];
+  for (let i = 0; i < scrapUntilPage; i++) {
+    if (i > 0) {
+      await page.evaluate(() => {
+        document
+          .querySelector(
+            '#app > section.results__container > div.results__wrapper > div.results__list.js-results > section > ul > li:nth-child(6) > button',
+          )
+          .click();
+      });
+    }
 
-    const list = imgArray.map( ({href}) => ({
-      href
-    }));
+    await page.waitForTimeout(2000);
 
-    console.log(list)
-  });
+    data[i] = await scrapPage(page);
+  }
+
+  console.log(data);
 
   // await browser.close();
 })();
+
+async function scrapPage(page) {
+  return await page.evaluate(() => {
+    const realStates = document.querySelectorAll(
+      '.results__wrapper .listings__container > div',
+    );
+    let dataIds = [];
+
+    realStates.forEach(realState => {
+      const dataId = realState.dataset['id'];
+
+      if (dataId) {
+        dataIds.push(dataId);
+      }
+    });
+
+    return dataIds;
+  });
+}
