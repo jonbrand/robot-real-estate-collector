44a6b8c22a25e6816ce2103c518b653e977cd454
tsuyusk <53716129+tsuyusk@users.noreply.github.com>
2021-11-16 18:54:58 -0300

    Change website.

--- src/index.js
+++ src/index.js
@@ -4,63 +4,69 @@ const { db } = require('./database/db');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-
+  const announceDatas = [];
   await page.goto(
     'https://www.vivareal.com.br/venda/rj/rio-de-janeiro/apartamento_residencial/',
   );
 
-  const announceDatas = await page.evaluate(async () => {
+  const pageLinks = await page.evaluate(() => {
     const selector =
       '#js-site-main > div.results__container > div.results__content > section > div.results-main__panel.js-list > div.results-list.js-results-list > div > div > article';
 
     const announces = [...document.querySelectorAll(selector)];
 
-    const announcesData = [];
+    const links = [];
 
-    let index = 0;
     for (const announce of announces) {
       const urlElement = announce.querySelector('a:nth-child(2)');
-      const priceElement = announce.querySelector(
-        'a:nth-child(2) > div > section > div > p',
+
+      if (urlElement) {
+        links.push(urlElement.href);
+      }
+    }
+
+    return links;
+  });
+
+  for (const pageLink of pageLinks) {
+    const newPage = await browser.newPage();
+    await newPage.goto(pageLink);
+
+    const announceData = await newPage.evaluate(pageLink => {
+      const titleElement = document.querySelector(
+        '#js-site-main > div.main-container > div.main-features-container > div.address-container > section > div > h1',
       );
-      const titleElement = announce.querySelector(
-        'a:nth-child(2) > div > h2 > span',
+      const priceElement = document.querySelector(
+        '#js-site-main > div.main-container > div.side-bar-container > div.price-container > div > div.price__content-wrapper > h3',
       );
 
-      announce.querySelector('.js-phone-lead').click();
+      /*document.querySelector('.js-phone-lead').click();
 
       await new Promise(r => setTimeout(r, 1000));
 
-      const phone = document.querySelector(
+      const phoneElement = document.querySelector(
         '#js-site-main > div.results__container > div.js-decision-lead-vue.vue-lead-form > div > div > section > p > a',
-      );
+      );*/
 
-      const announceData = {
-        url: urlElement.href,
-        price: priceElement ? priceElement.innerText : undefined,
+      return {
         title: titleElement ? titleElement.innerText : undefined,
-        phone: phone ? phone.innerText : undefined,
+        price: priceElement ? priceElement.innerText : undefined,
+        // phone: phoneElement ? phoneElement.innerText : undefined,
+        contact: '+5531996219283',
+        url: pageLink,
       };
+    }, pageLink);
 
-      announcesData.push(announceData);
+    announceDatas.push(announceData);
 
-      document
-        .querySelector(
-          '#js-site-main > div.results__container > div.js-decision-lead-vue.vue-lead-form > div > div > section > button',
-        )
-        .click();
+    await newPage.close();
 
-      index++;
-
-      await new Promise(r => setTimeout(r, 8000));
-    }
-
-    return await Promise.all(scrapAnnouncesPromises);
-  });
+    await new Promise(r => setTimeout(r, 3000));
+  }
 
   for (const announceData of announceDatas) {
     await db('real_estate').insert(announceData);
   }
 
-  console.log(announceDatas);
+  await browser.close();
 })();
